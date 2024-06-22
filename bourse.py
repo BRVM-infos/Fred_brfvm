859dea7a94a2c752c73f98ca30ae47e3803eb2cf
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-22 04:59:06 +0200

    Update bourse.py

--- bourse.py
+++ bourse.py
@@ -174,12 +174,13 @@ if selected_country:
 
 
     if selected_company:
-
+        ###########################################
         #Price Action calculate of the select company
         from action import extract_data_from_class as price # local python file
         test = "https://www.sikafinance.com/marches/cotation_"
         cac = df_main[df_main['Company_Name'] == selected_company]['Ticket'].unique()[0]
         url = test + cac
+        action = price(url)
         
         # Column Division
         cols = st.columns([0.8, 0.2], gap='medium')
