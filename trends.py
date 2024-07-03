4d8cc54806ac47dd176df997bba53e005c5cc184
BRVM-infos <alfreddiokou274@gmail.com>
2024-07-03 07:14:40 +0200

    Update trends.py

--- trends.py
+++ trends.py
@@ -4,8 +4,9 @@ import streamlit as st
 def app():
 
    st.markdown("""
-        <div">
+        <div class = logo1">
         <img src="https://github.com/ImageGalsen/Images/blob/67c82fe6fe5936b16deb8c14dba1419b0bf13fe5/soon.png?raw=true" class="logo" alt="Logo">
      </div>
         """, unsafe_allow_html=True)
+   
    
