9be4500c568aed8ed79c5af79ea667da68d0342d
BRVM-infos <alfreddiokou274@gmail.com>
2024-07-03 07:12:44 +0200

    Update trends.py
    coming soon logo

--- trends.py
+++ trends.py
@@ -3,5 +3,9 @@ import streamlit as st
 
 def app():
 
-    st.write("trends")
+   st.markdown("""
+        <div class="logo">
+        <img src="https://github.com/ImageGalsen/Images/blob/67c82fe6fe5936b16deb8c14dba1419b0bf13fe5/soon.png?raw=true" class="logo" alt="Logo">
+     </div>
+        """, unsafe_allow_html=True)
    
