4654c0c60e6adb05aa4583c41d607105f6a8f466
BRVM-infos <alfreddiokou274@gmail.com>
2024-07-03 07:35:20 +0200

    Update bourse.py

--- bourse.py
+++ bourse.py
@@ -122,8 +122,10 @@ class MultiApp:
         )
 
 ## To add more space
-        fuck = st.columns([0.1,0.6, 0.3], gap='small')
+        fuck = st.columns([0.1,0.8, 0.1], gap='small')
         with fuck[0]:
+            st.write("...")
+        with fuck[2]:
             st.write("...")
 
     # navigat page to choosse
